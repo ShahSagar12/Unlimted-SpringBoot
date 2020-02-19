4482f4cefaa3e47ffc68cc829e091b18138b339e
ShahSagar12 <oceenshah@gmail.com>
2020-02-19 17:17:27 +0545

    sagar feb19

---
+++ src/main/java/com/unlimited/UnlimitedSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.unlimited;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnlimitedSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnlimitedSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unlimited/UnlimitedSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unlimited;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnlimitedSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
